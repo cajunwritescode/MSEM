0f4e07d40be6ba2f72949aeb843e0dbcfbfe1ba4
cajun <12363371+CajunAvenger@users.noreply.github.com>
2025-12-31 23:21:16 -0600

    jan update

--- refuse.js
+++ refuse.js
@@ -1,20 +1,35 @@
 var fs = require('fs');
 
-var files = []
+var files = [];
+
+var downdate = process.argv.includes("-update");
 
 function refuse(fn) {
 	let card_files = [];
+	if(fn.match(/skip/)) {
+		setTimeout(() => {}, 10)
+		return;
+	}
 	let set_file = fs.readFileSync(`./Set Files/${fn}/set`, {encoding:'utf8'});
 	let lines = set_file.split("\n");
 	for(let l in lines) {
 		let include_match = lines[l].match(/^include[_ ]file: (card [^\n]+)/);
-		if(!include_match)
-			continue;
-		let cf = `./Set Files/${fn}/${include_match[1]}`;
-		card_files.push(cf);
-		let card_file = fs.readFileSync(cf, {encoding:'utf8'});
-		card_file = card_file.replace(/^[^\n]*mse[_ ]version:[^\n]+\n/, "");
-		lines[l] = card_file;
+		if(include_match) {
+			let cf = `./Set Files/${fn}/${include_match[1]}`;
+			card_files.push(cf);
+			let card_file = fs.readFileSync(cf, {encoding:'utf8'});
+			card_file = card_file.replace(/^[^\n]*mse[_ ]version:[^\n]+\n/, "");
+			lines[l] = card_file;
+		}
+		if(downdate) {
+			let update_match = lines[l].match(/!update/)
+			let patch_match = lines[l].match(/!update [^ :]: [^\n]+/);
+			if(patch_match) {
+				lines[l] = lines[l].replace(/!update [^ :]: [^\n]+/, "");
+			}else if(update_match) {
+				lines[l] = lines[l].replace(/!update/, "");
+			}
+		}
 	}
 	fs.writeFile(`./Set Files/${fn}/set`, lines.join("\n"), () => {
 		console.log(`./Set Files/${fn} fused`);
@@ -35,7 +50,12 @@ fs.readdir("./Set Files", (err, fns) => {
 	}
 })
 process.on('beforeExit', () => {
-	if(files.length)
-		refuse(files[0]);
+	if(files.length) {
+		try{
+			refuse(files[0]);
+		}catch(e) {
+			console.log(e);
+		}
+	}
 	files.splice(0,1);
 })
